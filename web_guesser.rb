f7efdb0aba6543deccdc01688e373e60b5c50d28
Leo <leoproechel@gmail.com>
2016-12-01 12:47:23 -0500

    I4: colorized output

--- web_guesser.rb
+++ web_guesser.rb
@@ -3,19 +3,27 @@ require 'sinatra/reloader'
 
 @@number = rand(101)
 @@initial_message = ""
+@@color = ""
 
 def check_guess(guess)
-	if guess.to_i > @@number + 5
+	if guess.to_i == 0
+		@@color = "white"
+	elsif guess.to_i > @@number + 5
 		message = "way too high"
+		@@color = "red"
 	elsif guess.to_i < @@number - 5
 		message = "way too low"
+		@@color = "red"
 	elsif guess.to_i > @@number
 		message = "too high"
+		@@color = "orange"
 	elsif guess.to_i < @@number
 		message = "too low"
+		@@color = "orange"
 	else
 		message = "correct!"
 		@@initial_message = "the SECRET NUMBER is #{@@number}"
+		@@color = "green"
 	end
 	message
 end
@@ -23,5 +31,5 @@ end
 get '/' do
 	guess = params["guess"]
 	message = check_guess(guess)
-	erb :index, :locals => {:number => @@number, :message => message, :initial_message => @@initial_message}
+	erb :index, :locals => {:number => @@number, :message => message, :initial_message => @@initial_message, :color => @@color}
 end
